606abdbdff1061cf54e1a53660c50958d560e04b
= <=>
2022-03-09 22:39:05 -0700

    loading tile map works, working on tweaks

--- dungeon-train/game/constants.py
+++ dungeon-train/game/constants.py
@@ -1,8 +1,19 @@
 # Write your constant variables here
+import os
+
+# store absolute path to game folder
+PATH = os.path.dirname(os.path.abspath(__file__))
 
 # screen dimensions
-SCREEN_WIDTH = 500
-SCREEN_HEIGHT = 500
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 400
 
 # window name
 SCREEN_TITLE = "Dungeon Train"
+
+# tilemap
+MAP = os.path.join(PATH, "..", "..", "assets", "map", "dungeon_map.tmx")
+
+# scaling
+TILE_SCALING = 1.5
+
--- dungeon-train/game/dungeon_train.py
+++ dungeon-train/game/dungeon_train.py
@@ -14,17 +14,51 @@ class Dungeon(arcade.Window):
             constants.SCREEN_TITLE
         )
 
+        # temporary background color
         arcade.set_background_color(arcade.color.BLACK)
-    
+
+        # scene object
+        self.scene = None
+
+        # tilemap object
+        self.tile_map = None
+
     def setup(self):
         """
         Setup Game Here. 
         Call this function to restart the game.
         """
-        pass
+        # name of the tilemap to load
+        map_name = constants.MAP
+
+        # Layer specific options are defined based on 
+        # Layer names in a dictionary. Doing this will 
+        # make the SpriteList for the platforms layer
+        # use spatial hashing for detection.
+        layer_options = {
+            "Base Layer": {
+                "use_spatial_hash": True,
+            },
+            "Layer 1": {
+                "use_spatial_hash": True,
+            },
+            "Layer 2": {
+                "use_spatial_hash": True,
+            },
+        }
+
+        # read the tiled map
+        self.tile_map = arcade.load_tilemap(map_name, constants.TILE_SCALING, layer_options)
+
+        # initialize scene with our tile map
+        self.scene = arcade.Scene.from_tilemap(self.tile_map)
+
 
     def on_draw(self):
         """
         Render the screen.
         """
         self.clear()
+
+        # draw our scene
+        self.scene.draw()
